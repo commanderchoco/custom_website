bf267d9e2b6b470a376c132f9aeba0763f29360b
Matthew Long <matthew.long@atlasschool.com>
2025-10-03 07:50:42 +0000

    added tooltip to scream button and changed image paths

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const toastTrigger = document.getElementById('screamToastBtn');
 const screamToast = document.getElementById('screamToast');
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 
 if (toastTrigger) {
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(screamToast)
